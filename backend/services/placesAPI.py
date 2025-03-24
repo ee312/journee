1f4b159ef377e93909d984c1c8464fef42426370
Emily <emily.ewing312@gmail.com>
2025-03-24 14:29:27 -0400

    placesAPI working

--- backend/services/placesAPI.py
+++ backend/services/placesAPI.py
@@ -1,14 +1,5 @@
 # google places API service
 
-# to find nearby activities: 
-
-# find place from text request (w/ user inputted city): 
-#   documentation: 
-#       https://developers.google.com/maps/documentation/places/web-service/search-find-place 
-# 
-#   https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
-#   can request geometry details - includes lat/long
-
 # find nearby places:
 #   documentation: 
 #       https://developers.google.com/maps/documentation/places/web-service/nearby-search
